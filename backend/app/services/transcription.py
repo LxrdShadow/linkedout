e77948e20afb1ceffdeccfe29b10d60a2e221ef2
LxrdShadow <andritiana.idealy@gmail.com>
2025-06-30 16:51:24 +0300

    chore(transcript): change transcription language from fr to en

--- backend/app/services/transcription.py
+++ backend/app/services/transcription.py
@@ -6,7 +6,7 @@ from app.core.config import ASSEMBLYAI_API_KEY
 
 aai.settings.api_key = ASSEMBLYAI_API_KEY
 
-config = aai.TranscriptionConfig(language_code="fr", speech_model=aai.SpeechModel.best)
+config = aai.TranscriptionConfig(language_code="en", speech_model=aai.SpeechModel.best)
 
 
 async def transcript_audio(audio_file: Union[str, BinaryIO]) -> aai.Transcript:
